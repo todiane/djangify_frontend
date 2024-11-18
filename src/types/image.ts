af929aa95e37e45a9d719226544e614e30f8e529
Diane Corriette <dcorriette@gmail.com>
2024-11-18 20:57:32 +0000

    Update image.ts file

--- src/types/image.ts
+++ src/types/image.ts
@@ -5,8 +5,14 @@ export interface ImageDimensions {
   quality: number;
 }
 
+// src/types/image.ts
+export interface ImageDimensions {
+  width: number;
+  height: number;
+  quality: number;
+}
+
 export interface ImageConfig {
-  blog: ImageDimensions;
   portfolio: {
     featured: ImageDimensions;
     gallery: ImageDimensions;
